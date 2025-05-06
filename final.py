7625caab507ab6a57364e8aeb261d6867043c358
jupiterIsa <juniper.osborn635@gmail.com>
2025-05-06 16:12:20 -0500

    set gravity values for player

--- final.py
+++ final.py
@@ -4,6 +4,11 @@ class Player():
     def __init__(self):
         self.pos = (400, 300)
         self.size = 15
+        self.surface = self.update_surface()
+        self.velocity = 400
+        self.jumpPower = 10
+        self.gravity = 0
+        self.gravityStrength = 10
 
 if __name__ == "__main__":
     main()
