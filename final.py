e9b1dc1e30a9c62dd27d0077f68cc83dd112836a
jupiterIsa <juniper.osborn635@gmail.com>
2025-05-08 13:59:59 -0500

    moved 47-48 back out of elif statement

--- final.py
+++ final.py
@@ -44,8 +44,8 @@ class Player():
             self.player_rect.topleft = self.pos
         
 
-            self.pos = (self.pos[0], self.pos[1] + dy)
-            self.player_rect.topleft = self.pos
+        self.pos = (self.pos[0], self.pos[1] + dy)
+        self.player_rect.topleft = self.pos
         
         if self.player_rect.colliderect(platform_rect):
             if dy > 0: 
